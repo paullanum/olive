1d292e9428fa5b4a33ecb789f1e151435a44ee3c
Paul Lanum <7023130+paullanum@users.noreply.github.com>
2023-01-16 15:07:51 -0800

    feat(buffer): Create buffer abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
+mod buffers;
 mod commands;
 mod input;
 
-use std::{
-    error::Error,
-    fs,
-    io::{self, BufRead},
-    path::PathBuf,
-};
+use std::{error::Error, io, path::PathBuf};
 
 use clap::Parser;
 use crossterm::{
@@ -14,12 +10,9 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use futures::{channel::mpsc::unbounded, future::select, pin_mut};
-use tui::{
-    backend::CrosstermBackend,
-    text::{Span, Spans, Text},
-    widgets::{Block, Borders, Paragraph},
-    Terminal,
-};
+use tui::{backend::CrosstermBackend, text::Text, Terminal};
+
+use crate::buffers::{Buffer, FileBuffer};
 
 /// A text editor trying its hardest to not edit text
 #[derive(Parser)]
@@ -45,28 +38,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let backend = CrosstermBackend::new(stdout);
     let mut term = Terminal::new(backend)?;
     if let Some(open_file) = cli.file {
-        if open_file.exists() {
-            let file = fs::File::open(&open_file)?;
-
-            let title = open_file
-                .file_name()
-                .unwrap_or_default()
-                .to_str()
-                .unwrap_or_default()
-                .to_string();
-
-            let buffer = Text {
-                lines: io::BufReader::new(file)
-                    .lines()
-                    .map(|s| Spans::from(vec![Span::from(s.unwrap_or_default())]))
-                    .collect::<Vec<_>>(),
-            };
-            let paragraph = Paragraph::new(buffer.lines)
-                .block(Block::default().borders(Borders::ALL).title(title));
-            term.draw(|f| f.render_widget(paragraph, f.size()))?;
-        } else {
-            dbg!("ERROR NO FILE");
-        }
+        FileBuffer::open(open_file)?.draw(&mut term)?;
     }
     let (tx, rx) = unbounded();
     let input_handler = input::InputHandler::new(tx);
